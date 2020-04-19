0c6c2cc9f50919659e4abe8659755e3b81822206
lo-b <bram_lo@outlook.com>
2020-04-19 23:41:19 +0200

    Change hyper parameters
    Change batch_size, number of epochs, alpha and add momentum.

--- src/main.py
+++ src/main.py
@@ -1,24 +1,32 @@
 from torch import optim
 import torch.nn as nn
 from src.model import Net
-from src.data import test_loader
 
 # Create the network and print it's architecture
 from src.train import train
+from src.visualize_tensorboard import write_images, write_embedding
 
 net = Net()
 print(net)
+
+# Visualize data in TensorBoard
+write_images()
+
+# TODO: check if below works on desktop
+# Line below doesn't work due to WebGL problems on laptop
+# write_embedding()
 
 # Specify the loss function; for a classification
 # problem with classes a `cross entropy loss`
 # works well.
 criterion = nn.CrossEntropyLoss()
 
+
 # Define learning rate `alpha`
 alpha = 0.01
 
 # Specify an optimizer which will do backpropagation
 # for us using autograd (a module that keeps track of a tensor's lifecycle)
-optimizer = optim.SGD(net.parameters(), lr=alpha)
+optimizer = optim.SGD(net.parameters(), lr=alpha, momentum=0.9)
 
-train(criterion, optimizer, 10, net)
+train(criterion, optimizer, 5, net)
