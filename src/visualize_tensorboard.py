e10ba6983162edcc787b4ceddcbfd61602970191
lo-b <bram_lo@outlook.com>
2020-06-06 20:31:37 +0200

    Show batch of predictions after training
    Add method for showing predictions by loading in a model checkpoint
    and making a prediction on a batch of images (20 total).

--- src/visualize_tensorboard.py
+++ src/visualize_tensorboard.py
@@ -2,17 +2,18 @@
 This module uses TensorBoard to visualize our data and to visualize
 our model so we can analyse it later.
 """
+import torch
+import numpy as np
 
+from src.data import train_data, classes, test_loader, train_loader
+from src.helpers import select_n_random
+import matplotlib.pyplot as plt
 import torchvision
 
-from src.data import train_data, classes
-from src.helpers import select_n_random
+from src.model import Net
 
 
 def write_images(writer):
-    # import DataLoader
-    from src.data import train_loader
-
     # get a batch of images
     images, labels = next(iter(train_loader))
 
@@ -42,3 +43,32 @@ def write_embedding(writer):
                          label_img=images.unsqueeze(1))
     writer.close()
     print("Write embedding done...")
+
+
+def visualize_test_results(device):
+    net = Net()
+    net.load_state_dict(torch.load('checkpoints/checkpoint_1.pt'))
+    net.to(device)
+    net.eval()
+
+    images, labels = next(iter(test_loader))
+
+    images = images.to(device)
+
+    probs = net.forward(images)
+
+
+    # get predictions
+    preds = np.squeeze(probs.data.max(1, keepdim=True)[1].cpu().numpy())
+    images = images.cpu().numpy()
+
+    # plot the images in the batch, along with predicted and true labels
+    fig = plt.figure(figsize=(25, 4))
+    for idx in np.arange(20):
+        ax = fig.add_subplot(2, 20 / 2, idx + 1, xticks=[], yticks=[])
+        ax.imshow(np.squeeze(images[idx]), cmap='gray')
+        ax.set_title(
+            "{} ({})".format(classes[preds[idx]], classes[labels[idx]]),
+            color=("green" if preds[idx] == labels[idx] else "red"))
+
+    fig.savefig('myfig.png')
